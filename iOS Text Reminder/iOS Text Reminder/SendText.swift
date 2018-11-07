226d7c077910ffacf1db7270c9dd98c64a670654
jonathansteward <jonathan.steward16@gmail.com>
2018-11-07 16:59:04 -0600

    <commit message>

---
+++ iOS Text Reminder/iOS Text Reminder/SendText.swift
@@ -0,0 +1,9 @@
+//
+//  SendText.swift
+//  iOS Text Reminder
+//
+//  Created by Jonathan Steward on 11/7/18.
+//  Copyright © 2018 Jonathan Steward. All rights reserved.
+//
+
+import Foundation
---
+++ iOS Text Reminder/iOS Text ReminderTests/iOS_Text_ReminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Text_ReminderTests.swift
+//  iOS Text ReminderTests
+//
+//  Created by Jonathan Steward on 11/7/18.
+//  Copyright © 2018 Jonathan Steward. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Text_Reminder
+
+class iOS_Text_ReminderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
