aa31933505c6654a683b4102511426ced5b85c5d
jonathansteward <jonathan.steward16@gmail.com>
2018-11-16 12:22:33 -0600

    Commiting Host and Task files

---
+++ iOS Text Reminder/iOS Text Reminder/Host.swift
@@ -0,0 +1,13 @@
+//
+//  Host.swift
+//  iOS Text Reminder
+//
+//  Created by Jonathan Steward on 11/16/18.
+//  Copyright © 2018 Jonathan Steward. All rights reserved.
+//
+
+import Foundation
+
+class Host {
+    
+}
---
+++ iOS Text Reminder/iOS Text Reminder/Tasks.swift
@@ -0,0 +1,13 @@
+//
+//  Tasks.swift
+//  iOS Text Reminder
+//
+//  Created by Jonathan Steward on 11/16/18.
+//  Copyright © 2018 Jonathan Steward. All rights reserved.
+//
+
+import Foundation
+
+class Tasks {
+    
+}
