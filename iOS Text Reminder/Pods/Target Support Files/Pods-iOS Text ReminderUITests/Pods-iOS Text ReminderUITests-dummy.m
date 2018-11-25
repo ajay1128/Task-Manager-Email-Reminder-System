8a0f086a9b21f2c1fcdcd51fc9fe488e1bfa3843
ajay1128 <32371218+ajay1128@users.noreply.github.com>
2018-11-25 15:10:00 -0600

    Added send text message functionality, via local server or twilio functions,  and needed pod and pod files

---
+++ iOS Text Reminder/Pods/Target Support Files/Pods-iOS Text ReminderUITests/Pods-iOS Text ReminderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Text_ReminderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Text_ReminderUITests
+@end
--- iOS Text Reminder/iOS Text Reminder/SendText.swift
+++ iOS Text Reminder/iOS Text Reminder/SendText.swift
@@ -1,5 +1,6 @@
 //
 //  SendText.swift
+//  - methods needed for sending text messages
 //  iOS Text Reminder
 //
 //  Created by Jonathan Steward on 11/7/18.
@@ -7,3 +8,60 @@
 //
 
 import Foundation
+import Alamofire
+
+//Send text messages via twilio functions
+//note - cannot delay twilio function for longer than 5 seconds
+//number - destination phone number
+//reminder - msg to user
+func sendText(number: String, msg: String) {
+    let headers = [
+        "Content-Type": "application/x-www-form-urlencoded"
+    ]
+    
+    let parameters: Parameters = [
+        "To":  number ,
+        "Body": msg
+        
+    ]
+    
+    Alamofire.request("https://black-locust-4228.twil.io/sms", method: .post, parameters: parameters, headers: headers).response { response in
+        print(response)
+        
+    }
+}
+
+
+//send text messages via local backend
+//allows the ability to delay text messages for a short time, but can only call send one at a time
+//
+
+//func sendTextServer(number: String, msg: String,time ) {
+//    let headers = [
+//        "Content-Type": "application/x-www-form-urlencoded"
+//    ]
+//
+//    let parameters: Parameters = [
+//        "To":  number ,
+//        "Body": msg
+//        "Time": time
+//
+//    ]
+//                      //link will change
+//    Alamofire.request("https://28f3b391.ngrok.io/sms", method: .post, parameters: parameters, headers: headers).response { response in
+//        print(response)
+//
+//    }
+//}
+
+
+
+// returns the number of seconds between the current time and the set reminder time, r
+// currentTime- current time
+// reminderTime - reminder time
+
+func timeCalc(currentTime: String, reminderTime: String)->CLongLong{
+    //stuff to do
+    return 0;
+}
+
